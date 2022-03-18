dcc09a9f6b11e854fd9ab660ec1c87a0a5b85a19
mike-kovtun <66732810+mike-kovtun@users.noreply.github.com>
2022-03-18 13:42:57 +0200

    Add files via upload

---
+++ Selectors.py
@@ -0,0 +1,17 @@
+# Logination
+login_page = "https://singit.io/auth/login"
+emailX = "//input[@type='email']"
+valid_mail = "auto-testing@singit.io"
+passwordX = "//input[@type='password']"
+valid_pass = "Aa123456"
+login_btn_ID = "main-button"
+# Elements of main page
+notif_iconX = "//*[@id='icon']"
+notif_menuX = "//*[@id='mat-menu-panel-0']"
+
+# Lesson/song page
+cont_less = "//*[@id='float']"
+
+# Word Pop-up
+more_lessID = "expand"
+
